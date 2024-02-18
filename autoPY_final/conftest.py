55fcbd0b628e4b2b912baa543817e7ca04a96fdb
fitoj11 <fitoj11@mail.ru>
2024-02-18 20:59:36 +0300

    start final project, create conftest and etc

---
+++ autoPY_final/conftest.py
@@ -0,0 +1,64 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+from selenium.webdriver.common.by import By # не влияет на запускаемые тесты, в каждом необходимо импортировать отдельно
+
+# @pytest.fixture(scope="function") # создали общую функцию для запуска браузера
+# def browser():
+#     print("\nstart browser for test..")
+#     browser = webdriver.Chrome()
+#     yield browser
+#     print("\nquit browser..")
+#     browser.quit()
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome', 
+                     help="Choose browser: chrome or firefox") # описываем аргумент, если оставим default=none - то при не заполнении аргумента browser_name будет падать с ошибкой
+    # parser.language('--language', action='store', default='en',
+    #     help="your language: en or us")
+
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: '--language=en' or '--language=ru'")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+
+    user_language = request.config.getoption("language")
+
+    options = Options()
+    options.add_experimental_option(
+        'prefs', {'intl.accept_languages': user_language})
+
+    options_firefox = OptionsFirefox()
+    options_firefox.set_preference("intl.accept_languages", user_language)
+
+    if browser_name == "chrome": # условие и значение переменной для браузера в запросе "pytest -s -v --browser_name=firefox test_fixture8.py"
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox") # обработчик, если значение будет иным
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome": # условие и значение переменной для браузера в запросе "pytest -s -v --browser_name=firefox test_fixture8.py"
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox") # обработчик, если значение будет иным
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
