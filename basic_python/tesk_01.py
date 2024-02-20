68f01904d238954fc33bab66c472fb1b363f32a1
aasorokin <fitoj11@mail.ru>
2024-02-20 10:44:24 +0300

    start basic_python leas

---
+++ basic_python/tesk_01.py
@@ -0,0 +1,28 @@
+# print("123")
+#
+# name = str(input('What your name '))
+# print("name is " + name)
+
+# x = 5
+# y = 3
+# print(x + y ** 2)
+# print(y ** 2 * x)
+# print(x ** y - x)
+# print(x ** (y - 2))
+# print(x / y + 3)
+# print(x / y * 3)
+# print(x // y)
+# print(x % y)
+# print(x == y or (x - 4) >= 1)
+# print(x != 2 and y <= 5)
+
+print(
+round(3.89),
+round(-2.49),
+round(4.999,2),
+abs(-5),
+abs(6.1),
+min(3, -2, -3.1, 0.2),
+max(1.9, -2, -3.1, 0.2),
+abs(min(3, -2, -3.1, 0.2)),
+min(3, abs(-2), abs(-3.1), 0.2))
