768e7987cd7d02b3067884bdfbf1e3bf2982d08b
aasorokin <fitoj11@mail.ru>
2024-02-21 15:11:35 +0300

    2.1. leasson, step 2, for / range leasson

--- basic_python/task_04.py
+++ basic_python/task_04.py
@@ -8,11 +8,31 @@ import math
 # t = float(input("t = "))
 # y = f_x(t)
 # print("f(", t, ") = ", y)
+
+# def vklad():
+#     x = float(input())
+#     k = float(input())
+#     n = float(input())
+#     s = x*((1+(k/(12*100)))**n)
+#     vklad_prib = s - x
+#     return vklad_prib
+# print(round(vklad()))
+
+# x_list = [-6, -7, -8, -9, -10]
+# for i in range(len(x_list)):
+#     print("x_list[%1d] = %5.2f" % (i, x_list[i]))
+# print(*list(range(0, 9, 2)))
+# print(*list(range(-6, -11, -1)))
+# print(*list(range(3, 13, 3)))
+# print(*list(range(0, 6)))
+
 def vklad():
-    x = float(input())
-    k = float(input())
-    n = float(input())
-    s = x*((1+(k/(12*100)))**n)
-    vklad_prib = s - x
-    return vklad_prib
+    for x in range(1000, 35000):
+        k = 5
+        n = 13
+        s = x*((1+(k/(12*100)))**n)
+        vklad_prib = s - x
+        if round(vklad_prib, 1) == 1859.0:
+            otvet = vklad_prib
+            return x
 print(round(vklad()))
