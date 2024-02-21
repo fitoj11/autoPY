d7f5388ad5cf24c81d71bdd305b8896a02c6afce
aasorokin <fitoj11@mail.ru>
2024-02-21 11:50:00 +0300

    1.4. leasson, step 3

--- basic_python/task_03.py
+++ basic_python/task_03.py
@@ -11,8 +11,20 @@ from math import log, sqrt, sin, pi, cos, tan, asin, acos, degrees
 # else:
 #     print('3')
 
-a=float(input())
-h=float(input())
-v=(a**2*h)/(4*sqrt(3))
-s=((a**2*sqrt(3))/4)+(((3*a)/2)*sqrt(h**2+(a**2/12)))
-print(round(v, 3), round(s, 3))
+# a=float(input())
+# h=float(input())
+# v=(a**2*h)/(4*sqrt(3))
+# s=((a**2*sqrt(3))/4)+(((3*a)/2)*sqrt(h**2+(a**2/12)))
+# print(round(v, 3), round(s, 3))
+
+year=int(input())
+if year < 0:
+    print('error')
+else:
+    if year % 4 == 0:
+        if year % 100 == 0 and year % 400 != 0:
+            print(365)
+        else:
+            print(366)
+    else:
+        print(365)
