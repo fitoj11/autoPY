ac9a390521e8ec86c6726804b7aaf9a01c8e3e1b
aasorokin <fitoj11@mail.ru>
2024-02-22 16:54:57 +0300

    2.3. leasson, step 4, [1:13]

--- basic_python/task_06.py
+++ basic_python/task_06.py
@@ -72,6 +72,7 @@ for line in f:
     t_list.append(float(t_lambda_list [0]))
     lambda_exp_list.append(float(t_lambda_list [1]))
 f.close()
+# t_list = t_list[1:13]
 lambda_list =[compute_lambda(t) for t in t_list]
 error_list = [abs((lambda_exp_list[i] - lambda_list[i]) / lambda_exp_list[i] )
               for i in range(len(t_list))]
@@ -82,7 +83,7 @@ for i in range(len(t_list)):
     print("|%7d | %7.3f | %7.1f |%7.2f%% |"
           % (t_list[i], lambda_list[i], lambda_exp_list[i], error_list[i] * 100))
 print("-" * 40)
-max_error = max(error_list)
+max_error = max(error_list[1:13])
 index_max_error = error_list.index(max_error)
 print("Максимальная погрешность = %5.2f%%  при t = %5d"
       % (max_error * 100, t_list[index_max_error]))
@@ -90,5 +91,5 @@ min_error = min(error_list)
 index_min_error = error_list.index(min_error)
 print("Минимальная погрешность = %5.2f%%  при t = %5d"
       % (min_error * 100, t_list[index_min_error]))
-avg_error = sum(error_list) / len(t_list)
+avg_error = sum(error_list[1:13]) / len(t_list[1:13])
 print("Средняя погрешность = %5.2f%%" % (avg_error * 100))
