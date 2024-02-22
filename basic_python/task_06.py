6f357c100bd60b4e9c84287fa1527c3f15d15062
aasorokin <fitoj11@mail.ru>
2024-02-22 15:00:23 +0300

    2.3. leasson, step 2

---
+++ basic_python/task_06.py
@@ -0,0 +1,23 @@
+def compute_lambda(t):
+    b = 33
+    l_0 = 884
+    t_0 = 100
+    return (b * l_0 /(t-t_0))
+def p_t(t):
+    return (s/n+(s-(t-1)*(s/n))*k/(12*100))
+s = int(input())
+n = int(input())
+k = int(input())
+if s <= 0 or n <= 0 or k <= 0:
+    print("error")
+else:
+    t_list = [i + 1 for i in range(0,n)]
+    lambda_list = [p_t(t) for t in t_list]
+    bank_profit = 0
+    for i in range(0,n):
+        print("%2d месяц - %8.2f руб" % (t_list[i], lambda_list[i]))
+        bank_profit = bank_profit + lambda_list[i-1]
+    print("Доход банка - %6.2f руб" % (bank_profit-s))
+
+
+
