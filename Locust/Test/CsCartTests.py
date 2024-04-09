4c488e8dc051b78ea5f8355f6971e50082860888
aasorokin <fitoj11@mail.ru>
2024-04-09 13:48:51 +0300

    End tests

--- Locust/Test/CsCartTests.py
+++ Locust/Test/CsCartTests.py
@@ -1,8 +1,10 @@
 from locust import HttpUser, task, between
 import random
 import re
+class FlowException(Exception):
+   pass
 class WebsiteTestUser(HttpUser):
-    wait_time = between(1, 1)
+    # wait_time = between(0, 0.01)
     def on_start(self): # запускает до старта locust
         global url
         global security_hash
@@ -10,20 +12,32 @@ class WebsiteTestUser(HttpUser):
         global security_hash_storefront2
         headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"}
         response = self.client.get("", headers=headers, name = 'Инициализация. Запрос на получение токена')
+        if response.status_code != 200:
+            raise FlowException('Not 200 status code')
         html_content = response.text
         csrf_token = re.search(r'<meta name="csrf-token" content="(.+?)">', html_content).group(1)
+        if csrf_token is None or csrf_token == "NoneType":
+            raise FlowException('None have object')
         form_data = {
             "_csrf": csrf_token,
             "edition": "ult",
             "type": "online"
         }
         response = self.client.post("", data=form_data, headers=headers, name = 'Инициализация. Создание демо магазина')
+        if response.status_code != 200:
+            raise FlowException('Not 200 status code')
         url = response.url
         html_content2 = response.text
         security_hash_storefront2 = re.search(r'name="security_hash" class="cm-no-hide-input" value="([^"]+)"', html_content2).group(1)
+        if security_hash_storefront2 is None or security_hash_storefront2 == "NoneType":
+            raise FlowException('None have object')
         response2 = self.client.get(f"{url}admin.php?dispatch=auth.login_form&return_url=admin.php", headers=headers, name = 'Инициализация. Получение security_hash')
+        if response2.status_code != 200:
+            raise FlowException('Not 200 status code')
         html_content2 = response2.text
         security_hash = re.search(r'name="security_hash" class="cm-no-hide-input" value="([^"]+)"', html_content2).group(1)
+        if security_hash is None or security_hash == "NoneType":
+            raise FlowException('None have object')
         form_data = {
             "return_url": "admin.php?dispatch=index.index",
             "user_login": "admin@example.com",
@@ -32,7 +46,9 @@ class WebsiteTestUser(HttpUser):
             "security_hash": security_hash
         }
         print(f"{url}admin.php")
-        self.client.post(f"{url}admin.php", data=form_data, headers=headers, name = 'Инициализация. Авторизация в админку')
+        response = self.client.post(f"{url}admin.php", data=form_data, headers=headers, name = 'Инициализация. Авторизация в админку')
+        if response.status_code != 200:
+            raise FlowException('Not 200 status code')
         form_data2 = {
             "full_render": "true",
             "security_hash": security_hash,
@@ -45,14 +61,20 @@ class WebsiteTestUser(HttpUser):
         pass
     @task(1) # Запросы главной страницы
     def home_page(self):
-        self.client.get(url, name = 'Главная страница. Запрос главной страницы')
+        response = self.client.get(url, name = 'Главная страница. Запрос главной страницы')
+        if response.status_code != 200:
+            raise FlowException('Not 200 status code')
     @task(2) # Регистрация
     def registration(self):
-        self.client.get(f"{url}index.php?dispatch=auth.logout", headers=headers, name = 'Регистрация. Выход из аккаунта (перед регистрацией)')  ## logout
+        response = self.client.get(f"{url}index.php?dispatch=auth.logout", headers=headers, name = 'Регистрация. Выход из аккаунта (перед регистрацией)')  ## logout
+        if response.status_code != 200:
+            raise FlowException('Not 200 status code')
         number = random.randint(0, 100000)
         response2 = self.client.get(f"{url}profiles-add", headers=headers, name = 'Регистрация. Получение security_hash_storefront')
         html_content2 = response2.text
         security_hash_storefront1 = re.search(r'name="security_hash" class="cm-no-hide-input" value="([^"]+)"', html_content2).group(1)
+        if security_hash_storefront1 is None or security_hash_storefront1 == "NoneType":
+            raise FlowException('None have object')
         form_data = {
             "ship_to_another": "",
             "user_data[firstname]": f"{number}",
@@ -66,13 +88,18 @@ class WebsiteTestUser(HttpUser):
             "dispatch[profiles.update]": "",
             "security_hash": security_hash_storefront1}
         response = self.client.post(url, data=form_data, headers=headers, name = 'Регистрация. Регистрация')
+        if response.status_code != 200:
+            raise FlowException('Not 200 status code')
         try:
             html_content2 = response.text # выводит email если залогинены
             account_number = re.search(r'"ty-account-info__item  ty-account-info__name ty-dropdown-box__item">(.*?)</li>', html_content2).group(1)
+            if account_number == None or account_number == "NoneType":
+                raise FlowException('None have object')
             if int(account_number) == number:
                 print("Account name: ", number)
         except:
-            print("Not registred")
+            raise FlowException('Account not registred')
+        pass
     @task(3) # запрос всего каталога
     def catalog(self):
         params = {
@@ -87,12 +114,19 @@ class WebsiteTestUser(HttpUser):
               "hint_q": "Искать товары",
               "dispatch": "products.search",
               "security_hash": f"{security_hash_storefront2}"}
-        self.client.get(url, headers=headers, params=params, name='Запрос каталога. Запрос всех товаров')
+        response = self.client.get(url, headers=headers, params=params, name='Запрос каталога. Запрос всех товаров')
+        if response.status_code != 200:
+            raise FlowException('Not 200 status code')
+        pass
     @task(4) # Добавить товар в корзину, оформить заказ
     def home_page(self):
         response2 = self.client.get(f"{url}index.php?dispatch=auth.logout", headers=headers, name = 'Оформление заказа. Выход из аккаунта (перед оформлением заказа)')
+        if response2.status_code != 200:
+            raise FlowException('Not 200 status code')
         html_content2 = response2.text
         security_hash_storefront1 = re.search(r'name="security_hash" class="cm-no-hide-input" value="([^"]+)"', html_content2).group(1)
+        if security_hash_storefront1 is None or security_hash_storefront1 == "NoneType":
+            raise FlowException('None have object')
         form_data = {
             "result_ids": "cart_status *, wish_list *, checkout *, account_info *",
             "redirect_url": "index.php?dispatch = products.view & product_id = 278",
@@ -136,7 +170,9 @@ class WebsiteTestUser(HttpUser):
             "full_render": "Y",
             "is_ajax": "1",
             "dispatch[checkout.add..278]": ""}
-        self.client.post(url, data=form_data, headers=headers, name = 'Оформление заказа. Добавить товар в корзину')
+        response = self.client.post(url, data=form_data, headers=headers, name = 'Оформление заказа. Добавить товар в корзину')
+        if response.status_code != 200:
+            raise FlowException('Not 200 status code')
         number = random.randint(0, 100000)
         form_data = {
             "ship_to_another": "1",
@@ -168,7 +204,21 @@ class WebsiteTestUser(HttpUser):
             "security_hash": security_hash_storefront1
         }
         response = self.client.post(url, data=form_data, headers=headers, name = 'Оформление заказа. Оформить заказ')
+        if response.status_code != 200:
+            raise FlowException('Not 200 status code')
         html_content2 = response.text
-        order_id = re.search(r'dispatch=checkout.complete&amp;([^"]+)"', html_content2).group(1)
+        try:
+            order_id_match = re.search(r'dispatch=checkout.complete&amp;([^"]+)"', html_content2)
+            if order_id_match is None:
+                raise FlowException('None have object')
+
+            order_id = order_id_match.group(1)
+            if order_id == "NoneType":
+                raise FlowException('Order not success')
+        except FlowException as e:
+            raise e
+        except Exception as ex:
+            raise FlowException('Unexpected error occurred: {}'.format(ex))
         print("Order, account name: ", number)
         print(order_id)
+        pass
